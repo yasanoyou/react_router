ce042f69969d511baa95b20dd6c66b63f9f6ed11
yasanoyou <wearejanne2007@icloud.com>
2021-11-29 00:19:59 +0900

    ページ変異でstateを渡す

--- src/Page1DetailA.jsx
+++ src/Page1DetailA.jsx
@@ -1,4 +1,9 @@
+import { useLocation } from "react-router-dom";
+
 export const Page1DetailA = () => {
+  const { state } = useLocation();
+  console.log(state);
+
   return(
     <h1>Page1DetailAページです</h1>
   );
