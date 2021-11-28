cefe7831ac9e065650c07d4c1f3e873b972fec0a
yasanoyou <wearejanne2007@icloud.com>
2021-11-29 00:29:47 +0900

    historyによる画面遷移

--- src/Page1DetailA.jsx
+++ src/Page1DetailA.jsx
@@ -1,10 +1,17 @@
-import { useLocation } from "react-router-dom";
+import { useLocation, useHistory } from "react-router-dom";
 
 export const Page1DetailA = () => {
   const { state } = useLocation();
   console.log(state);
 
+  const history = useHistory();
+
+  const onClickBack = () => history.goBack();
+
   return(
-    <h1>Page1DetailAページです</h1>
+    <div>
+      <h1>Page1DetailAページです</h1>
+      <button onclick={onClickBack}>戻る</button>
+    </div>
   );
 };
